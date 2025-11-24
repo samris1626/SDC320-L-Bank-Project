e1473302ccb3e3cc8a52e84d46842462ddf868f2
samris1626 <samris1626@students.ecpi.edu>
2025-11-23 22:35:19 -0500

    Update assignment details and implement ITransaction

--- CheckingAccount.cs
+++ CheckingAccount.cs
@@ -1,16 +1,27 @@
 /*********************************************
 * Name: Samantha Riser
-* Date: 11/18/2025
-* Assignment: SDC320L - WK 1 - 1.5 Project
+* Date: 11/23/2025
+* Assignment: SDC320L - WK 2 - 2.2 Project
 *
 * Class for checking accounts.
 */
 
 namespace BankProject
 {
-    public class CheckingAccount : Account
+    public class CheckingAccount : Account, ITransaction
     {
         public CheckingAccount(string ownerName, ContactInfo contact, decimal balance)
             : base(ownerName, contact, balance) { }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+                Balance -= amount;
+        }
     }
 }
