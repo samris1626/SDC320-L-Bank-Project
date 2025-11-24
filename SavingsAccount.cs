4d919aae823fcd16f37286f263462c94224c0a98
samris1626 <samris1626@students.ecpi.edu>
2025-11-23 22:36:15 -0500

    Modify SavingsAccount class and update comments
    Updated assignment details and added ITransaction interface implementation.

--- SavingsAccount.cs
+++ SavingsAccount.cs
@@ -1,17 +1,27 @@
 /*********************************************
 * Name: Samantha Riser
-* Date: 11/18/2025
-* Assignment: SDC320L - WK 1 - 1.5 Project
+* Date: 11/23/2025
+* Assignment: SDC320L - WK 2 - 2.2 Project
 *
 * Class for savings accounts.
 */
 
 namespace BankProject
 {
-    // Inheritance: SavingsAccount derives from Account.
-    public class SavingsAccount : Account
+    public class SavingsAccount : Account, ITransaction
     {
         public SavingsAccount(string ownerName, ContactInfo contact, decimal balance)
             : base(ownerName, contact, balance) { }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+                Balance -= amount;
+        }
     }
 }
