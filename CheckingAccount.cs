701b0bf13e6133b324545c550c2eb454a2579682
samris1626 <samris1626@students.ecpi.edu>
2025-12-08 23:50:59 -0500

    Refactor CheckingAccount class for brevity

--- CheckingAccount.cs
+++ CheckingAccount.cs
@@ -1,32 +1,23 @@
 /*********************************************
 * Name: Samantha Riser
-* Date: 11/30/2025
-* Assignment: SDC320L - WK 3
+* Date: 12/08/2025
+* Assignment: SDC320L - WK 4
 *
 * Checking account class.
 * Demonstrates inheritance, abstraction, constructors,
 * and proper access specifiers.
-*********************************************/
+*/
 
 namespace BankProject
 {
     public class CheckingAccount : Account, ITransaction
     {
-        // Default constructor
-        public CheckingAccount() : base() 
-        {
-        }
+        public CheckingAccount() : base() { }
 
-        // Parameterized constructor
         public CheckingAccount(string ownerName, ContactInfo contact, decimal balance)
-            : base(ownerName, contact, balance)
-        {
-        }
+            : base(ownerName, contact, balance) { }
 
-        public void Deposit(decimal amount)
-        {
-            _balance += amount;
-        }
+        public void Deposit(decimal amount) => _balance += amount;
 
         public void Withdraw(decimal amount)
         {
@@ -34,7 +25,6 @@ namespace BankProject
                 _balance -= amount;
         }
 
-        // Required abstract method implementation
         public override string GetAccountDetails()
         {
             return $"Checking Account for {OwnerName}. Current Balance: {_balance:C}";
