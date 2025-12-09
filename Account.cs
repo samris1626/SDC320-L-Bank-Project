5a0b0bbb72418ffce85526313483f2dc56cbc05e
samris1626 <samris1626@students.ecpi.edu>
2025-12-08 23:50:17 -0500

    Update date and assignment information in Account.cs

--- Account.cs
+++ Account.cs
@@ -1,22 +1,19 @@
 /*********************************************
 * Name: Samantha Riser
-* Date: 11/30/2025
-* Assignment: SDC320L - WK 3 - Phase 3
+* Date: 12/08/2025
+* Assignment: SDC320L - WK 4
 *
 * Abstract base class for all bank account types.
 * Demonstrates abstraction, constructors, and
 * proper use of access specifiers.
-*********************************************/
+*/
 
 namespace BankProject
 {
     public abstract class Account
     {
-        // Private fields (not directly accessible outside class)
         private string _ownerName;
         private ContactInfo _contact;
-
-        // Protected: accessible by derived classes
         protected decimal _balance;
 
         public string OwnerName
@@ -33,14 +30,13 @@ namespace BankProject
 
         public decimal Balance => _balance;
 
-         public Account()
+        public Account()
         {
             _ownerName = "Unknown";
             _contact = new ContactInfo("N/A", "N/A", "N/A");
             _balance = 0m;
         }
 
-        // Parameterized constructor
         public Account(string ownerName, ContactInfo contact, decimal balance)
         {
             _ownerName = ownerName;
